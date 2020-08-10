5a1729eb5baab8873ac936615412a0c5deaa7546
Rodrigo P <50590@sistemas.frc.utn.edu.ar>
2020-08-09 22:50:22 -0300

    fixes context

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	ctx := context.WithValue(context.Background(), "userID", 12345)
+	ctx = context.WithValue(ctx, 1, "admin")
+
+	if v := ctx.Value("userID"); v != nil{
+		fmt.Println(v)
+	} else {
+		fmt.Println("no value associates with that key")
+	}
+
+	if v := ctx.Value(1); v != nil{
+		fmt.Println(v)
+	} else {
+		fmt.Println("no value associates with that key")
+	}
+
+	if v := ctx.Value(2); v != nil{
+		fmt.Println(v)
+	} else {
+		fmt.Println("no value associates with that key")
+	}
+}
