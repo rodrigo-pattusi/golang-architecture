71561cc777a04ac222276030ee2fc87f69b5f00e
Rodrigo P <50590@sistemas.frc.utn.edu.ar>
2020-08-10 17:51:00 -0300

    second try

--- main.go
+++ main.go
@@ -8,13 +8,15 @@ import (
 
 func main() {
 	ctx := context.Background()
-	ctx, cancelF := context.WithTimeout(ctx, time.Second)
+	ctx, cancelF := context.WithTimeout(ctx, 500 * time.Millisecond)
 	defer cancelF()
-	time.Sleep(500 * time.Millisecond)
+
+	time.Sleep(100 * time.Millisecond)
+
 	select {
-	case <- ctx.Done():
-		fmt.Println("not finished")
+	case <-ctx.Done():
+		fmt.Println("work didn't finish")
 	default:
-		fmt.Println("finished")
+		fmt.Println("work finished")
 	}
 }
