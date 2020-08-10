f71f2f07b35e17f3b0a02f28776167706bf97814
Rodrigo P <50590@sistemas.frc.utn.edu.ar>
2020-08-10 17:34:48 -0300

    firsttry

--- main.go
+++ main.go
@@ -3,23 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/rodrigo-pattusi/golang-architecture/session"
+	"time"
 )
 
 func main() {
 	ctx := context.Background()
-
-	ctx = session.SetUserId(ctx, 1)
-	ctx = session.SetIsAdmin(ctx, true)
-
-	uID := session.GetUserId(ctx)
-	isAdmin := session.GetIsAdmin(ctx)
-	fmt.Printf("User %d is an admin %t", uID, isAdmin)
-
-	ctx = session.SetUserId(ctx, 1)
-	ctx = session.SetIsAdmin(ctx, true)
-	i := session.GetUserId(ctx)
-	b := session.GetIsAdmin(ctx)
-	fmt.Printf("user %d is admin %t\n", i, b)
-
+	ctx, cancelF := context.WithTimeout(ctx, time.Second)
+	defer cancelF()
+	time.Sleep(500 * time.Millisecond)
+	select {
+	case <- ctx.Done():
+		fmt.Println("not finished")
+	default:
+		fmt.Println("finished")
+	}
 }
